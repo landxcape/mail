6ff487747b81e404c04a0a7f73d5bfbbe95c9f44
Ashish Shakya <38076182+landxcape@users.noreply.github.com>
2020-10-06 15:15:31 +0545

    send mail complete

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector('#compose-view').addEventListener('submit', send_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -21,6 +20,8 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-form').onsubmit = () => send_email();
 }
 
 function load_mailbox(mailbox) {
@@ -31,23 +32,62 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Show emails
+  const emails_view = document.querySelector('#emails-view');
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      emails.forEach(email => {
+        var emails_container = document.createElement('div');
+        var email_sender = document.createElement('div');
+        var email_subject = document.createElement('div');
+        var email_timestamp = document.createElement('div');
+
+        emails_container.classList.add('row', 'border', 'border-dark', 'my-2', 'py-2');
+        email_sender.classList.add('col-md-3', 'col-sm-6', 'overflow-hidden');
+        email_subject.classList.add('col-md-6', 'col-sm-6', 'overflow-hidden');
+        email_timestamp.classList.add('col-md-3', 'col-sm-6', 'overflow-hidden', 'd-flex', 'justify-content-end');
+
+        email.recipients.forEach(recipient => {
+          var recipients_div = document.createElement('div');
+          recipients_div.classList.add('row', 'mx-0');
+          recipients_div.innerHTML = mailbox === 'sent' ? `To: ${recipient}` : email.sender;
+          email_sender.appendChild(recipients_div)
+        });
+
+        email_subject.innerHTML = email.subject;
+        email_timestamp.innerHTML = email.timestamp;
+
+        emails_container.appendChild(email_sender);
+        emails_container.appendChild(email_subject);
+        emails_container.appendChild(email_timestamp);
+        emails_view.appendChild(emails_container);
+      });
+    });
 }
 
 function send_email() {
-  const compose_recipients = document.querySelector('#compose-recipients').value;
-  const compose_subject = document.querySelector('#compose-subject').value;
-  const compose_body = document.querySelector('#compose-body').value;
+  const compose_recipients = document.querySelector('#compose-recipients');
+  const compose_subject = document.querySelector('#compose-subject');
+  const compose_body = document.querySelector('#compose-body');
+
+  console.log(compose_recipients.value);
+  console.log(compose_subject.value);
+  console.log(compose_body.value);
 
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-      recipients: compose_recipients,
-      subject: compose_subject,
-      body: compose_body
+      recipients: compose_recipients.value,
+      subject: compose_subject.value,
+      body: compose_body.value
     })
   })
     .then(response => response.json())
     .then(result => {
-      console.log(result);
+      alert(Object.values(result))
+      load_mailbox('sent');
     });
+  return false;
 }
